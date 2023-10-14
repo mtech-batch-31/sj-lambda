7cac7599a45b3952a7b80765cca2766b37dad47f
linxiaoxin <lin_xiaoxin@hotmail.com>
2023-10-14 19:18:43 +0800

    Update Lamdba job seeker update

--- lambda_function.py
+++ lambda_function.py
@@ -4,13 +4,13 @@ import os
 
 print('Loading function')
  # Get environment variables
-DB_SERVER = os.getenv('DB_SERVER')250
+DB_SERVER = os.getenv('DB_SERVER')
 DB_PORT= os.environ.get('DB_PORT')
 DB_NAME= os.environ.get('DB_NAME')
 DB_PASSWORD= os.environ.get('DB_PASSWORD')
 DB_USERNAME = os.environ.get('DB_USERNAME')
 
-connection = psycopg2.connect(host= f"{DB_SERVER};{DB_PORT}",database=DB_NAME, user=DB_USERNAME, password=DB_PASSWORD)
+connection = psycopg2.connect(host=DB_SERVER, port=DB_PORT,dbname=DB_NAME, user=DB_USERNAME, password=DB_PASSWORD)
 connection.set_session(autocommit=True)
 
 def lambda_handler(event, context):
@@ -22,6 +22,8 @@ def lambda_handler(event, context):
     for record in event['Records']:
         jsonbody=(record["body"])
         status_update=json.loads(jsonbody)
+        print(status_update)
+        process_status_update(status_update)
 
     return {
         'statusCode': 200,
@@ -29,18 +31,22 @@ def lambda_handler(event, context):
     }
 
 def process_status_update(status_msg):
-    username = status_msg.accountUuid
-    status = status_msg.seekingJob
-    update_time = status_msg.sendTime
+    username = status_msg["accountUuid"]
+    status = status_msg["seekingJob"]
+    update_time = status_msg["sendTime"]
     update_job_app_status(username, status, update_time)
    
 
 def update_job_app_status(username, status, update_time):
-      connection.execute("""
+      with connection.cursor() as cursor:
+        cursor.execute("""
         UPDATE sjmsjob.job_application
-        SET seeker_status = status, seeker_status_last_updated_date = update_time
-        WHERE user_id = username and seeker_status_last_updated_date < update_time""", {
+        SET seeker_status = %(status)s
+        WHERE user_id = %(username)s""", {
            'username': username,
            'status': status,
            'update_time': update_time
         });
+      connection.commit()
+      print("updated db")
+
